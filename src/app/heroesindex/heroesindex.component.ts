f52d77011a50c4f83f80b125cd391ba0f420a39d
Shane Swezey <shanedswezey@gmail.com>
2018-09-24 18:02:41 -0700

    Code clean up

--- src/app/heroesindex/heroesindex.component.ts
+++ src/app/heroesindex/heroesindex.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { HeroapiService } from '../services/heroapi.service';
 import { Hero } from '../models/Hero';
 import { Observable } from 'rxjs/Observable';
@@ -12,6 +12,7 @@ import { Router } from '@angular/router';
 })
 export class HeroesindexComponent implements OnInit {
 
+  @ViewChild('heroButton') resetButton: ElementRef;
   private heroList: Observable<Hero>;
 
   constructor(private heroService: HeroapiService, private router: Router) { }
